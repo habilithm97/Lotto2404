f71c9e3709aca95b8d82a87acebd5c59d7b7ba0a
habilithm97 <habinkim97@gmail.com>
2024-04-11 17:32:01 +0900

    랜덤 로또 번호 생성

--- app/src/main/java/com/example/lotto2404/Fragment1.kt
+++ app/src/main/java/com/example/lotto2404/Fragment1.kt
@@ -1,11 +1,13 @@
 package com.example.lotto2404
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.lotto2404.databinding.Fragment1Binding
+import kotlin.random.Random
 
 class Fragment1 : Fragment() {
     private lateinit var binding: Fragment1Binding
@@ -17,6 +19,18 @@ class Fragment1 : Fragment() {
     }
 
     private fun init() {
+        binding.btn.setOnClickListener {
+            val lottoNums = createLottoNumber()
+            Log.d("Fragment1", lottoNums.toString())
+        }
+    }
 
+    private fun createLottoNumber() : List<Int> {
+        val result = mutableListOf<Int>()
+        val nums = IntArray(45) {it + 1}
+        nums.shuffle(Random(System.currentTimeMillis()))
+        nums.slice(0..5).forEach { num -> result.add(num) }
+        result.sort()
+        return result
     }
 }
